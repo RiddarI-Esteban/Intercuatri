efe6ccf28ee287ea0c6e27dc67d39beeda3712f7
Esteban.Salado <181036@utags.edu.mx>
2020-12-17 09:02:06 -0600

    Cuarto commit guardado

--- server/models/departamento.js
+++ server/models/departamento.js
@@ -7,20 +7,24 @@ type: String,
 required: [true, 'Coloque su nombre aqui']
     },
 
+    
     numero_empleados: {
 type: Number,
 required: [true, 'Coloque el numero de empleados aqui']
     },
+
 
     extension_telefonica: {
 type: Number,
 required: [true, 'Coloque su extencion telefonica aqui']
     },
 
+
     activo: {
 type: Boolean,
 default: true
     },
+
 
     id_jefe_de_area: {
 type: Schema.Types.ObjectId,
